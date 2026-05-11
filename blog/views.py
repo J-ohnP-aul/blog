6605a6e0e0016ed4b501aa7720b2cbf199be340f
J-ohnP-aul <johnpaulnoblemen@gmail.com>
2026-05-11 23:36:56 +0300

    done with the email system

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.views.generic import ListView #cbv
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 from .models import Post
 
@@ -52,10 +53,30 @@ def post_share(request, post_id):
     id=post_id,
     status = Post.Status.PUBLISHED
   )
+  sent = False
   if request.method=='POST':
     form = EmailPostForm(request.POST)
     if form.is_valid():
       cd = form.cleaned_data #clean
+      post_url = request.build_absolute_uri(
+        post.get_absolute_url()
+      )
+      subject = (
+        f"{cd['name']}  ({cd['email']})"
+        f"recommends you to read {post.title}"
+      )
+      message = (
+        f"Read {post.title} at {post_url}\n\n"
+        f"{cd['name']}\'s comments: {cd['comments']}"
+      )
+      send_mail(
+        subject=subject,
+        message=message,
+        from_email=None,
+        recipient_list=[cd['to']]
+      )
+      sent = True
   else: 
     form = EmailPostForm()
-  return render('blog/post/share.html', {'post':post, 'form':form})
+  return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent},)
+
