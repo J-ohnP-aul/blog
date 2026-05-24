7a7759a71168db17b1e53777fe5e7654832f87f1
J-ohnP-aul <johnpaulnoblemen@gmail.com>
2026-05-24 19:19:37 +0300

    added tag system

--- blog/views.py
+++ blog/views.py
@@ -10,13 +10,6 @@ from .models import Post
 
 # Create your views here.
 
-# class PostListView(ListView):
-#   """alternative postlist view"""
-#   queryset = Post.published.all()
-#   context_object_name = 'post'
-#   paginate_by = 3
-#   template_name = 'blog/post/list.html'
-
 def post_list(request):
   post_list = Post.published.all()
   
@@ -31,6 +24,11 @@ def post_list(request):
 
   return render(request, 'blog/post/list.html', {'posts':posts})
 
+# class PostListView(ListView):
+#   queryset = Post.published.all()
+#   content_object_name = 'posts'
+#   paginate_by = 3
+#   template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
   post = get_object_or_404(
@@ -46,38 +44,6 @@ def post_detail(request, year, month, day, post):
   form = CommentForm() #form 4 user 2 comment
   return render(request, 'blog/post/detail.html', {'post':post, 'comments':comments, 'form':form})
 
-def post_share(request, post_id):
-  post = get_object_or_404(
-    Post,
-    id=post_id,
-    status = Post.Status.PUBLISHED
-  )
-  sent = False
-  if request.method=='POST':
-    form = EmailPostForm(request.POST)
-    if form.is_valid():
-      cd = form.cleaned_data #clean
-      post_url = request.build_absolute_uri(
-        post.get_absolute_url()
-      )
-      subject = (
-        f"{cd['name']}  ({cd['email']})"
-        f"recommends you to read {post.title}"
-      )
-      message = (
-        f"Read {post.title} at {post_url}\n\n"
-        f"{cd['name']}\'s comments: {cd['comments']}"
-      )
-      send_mail(
-        subject=subject,
-        message=message,
-        from_email=None,
-        recipient_list=[cd['to']]
-      )
-      sent = True
-  else: 
-    form = EmailPostForm()
-  return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent},)
 
 @require_POST
 def post_comment(request, post_id):
@@ -101,3 +67,38 @@ def post_comment(request, post_id):
       'comment':comment
     }
   )
+  
+def post_share(request, post_id):
+  post = get_object_or_404(
+    Post,
+    id = post_id,
+    status=Post.Status.PUBLISHED
+  )
+  sent = False
+  if request.method=='POST':
+    form = EmailPostForm(request.POST)
+    if form.is_valid():
+      cd  = form.cleaned_data
+      post_url = request.build_absolute_uri(
+        post.get_absolute_url()
+      )
+      subject = (
+        f"{cd['name']} ({cd['email']})"
+        f"recommends you read {post.title}"
+      )
+      message = (
+        f"Read {post.title} at {post_url}\n\n"
+        f"{cd['name']}\'s comments: {cd['comments']}"
+      )
+      send_mail(
+        subject=subject,
+        message=message,
+        from_email=None,
+        recipient_list=[cd['to']]
+      )      
+      sent =True
+  else:
+    form = EmailPostForm()
+  return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent},)
+      
+        
